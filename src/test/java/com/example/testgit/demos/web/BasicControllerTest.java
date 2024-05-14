43a80a33a9c9ced8fb458de0bdc924efd1065020
JudyGod <zjuzhp@zju.edu.cn>
2024-05-14 23:39:54 +0800

    test01

---
+++ src/test/java/com/example/testgit/demos/web/BasicControllerTest.java
@@ -0,0 +1,34 @@
+package com.example.testgit.demos.web;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @Description: TODO
+ * @Author: Scathach
+ * @Aate: 2024/5/14 23:39
+ * @version: V1.0
+ */
+class BasicControllerTest {
+
+    @Test
+    void hello() {
+    }
+
+    @Test
+    void user() {
+    }
+
+    @Test
+    void saveUser() {
+    }
+
+    @Test
+    void html() {
+    }
+
+    @Test
+    void parseUser() {
+    }
+}
